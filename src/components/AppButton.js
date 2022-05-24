157b2db1583c53ed7d08b4e5a41ad8a3f2923d03
Gabo3900 <zinho3900@gmail.com>
2022-05-24 15:39:19 -0300

    adicionando componentes internos

---
+++ src/components/AppButton.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const AppButton = props => (<div>
+    <input type="submit" title="Entrar" />
+</div>);
---
+++ src/components/AppInput.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const AppInput = ({ label, type, title }) => (<div>
+    <label>{label}</label>
+    <input type={type} title={title} />
+</div>);
